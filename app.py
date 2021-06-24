202405c3bc97db3e85a83b0ed7fbeefe16e4c243
sepehr <ahmadi.sepehr.89@gmail.com>
2021-06-23 21:59:32 -0700

    contact.css was added and app is modified

--- app.py
+++ app.py
@@ -1,12 +1,42 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
+
+app = Flask(__name__, template_folder='templates')
 
 
-app = Flask(__name__,template_folder='templates')
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html')
 
 
 @app.route('/about_us')
 def about_us():
     return render_template('about_us.html')
+
+
+@app.route("/menu")
+def menu():
+    return render_template('menu.html')
+
+
+@app.route("/Receipts")
+def receipts():
+    return render_template('Receipts.html')
+
+
+@app.route("/Receipts_print")
+def recepits_print():
+    return render_template('Recepits-print.html')
+
+
+@app.route("/table")
+def table():
+    return render_template('table.html')
+
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
 
 
 if __name__ == '__main__':
